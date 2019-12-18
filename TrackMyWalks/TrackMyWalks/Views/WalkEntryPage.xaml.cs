e0a7633b573e644d4d4fe13b4457a0d33ab05112
Manuel Valtingojer <m.valtingojer@gasmotoren.at>
2019-12-18 15:00:48 +0100

    Add project files.

---
+++ TrackMyWalks/TrackMyWalks/Views/WalkEntryPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TrackMyWalks.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WalkEntryPage : ContentPage
+    {
+        public WalkEntryPage()
+        {
+            InitializeComponent();
+
+            // Update the page title for the walks entry page
+            Title = "New Walk Entry Page";
+        }
+
+        // Instance method that saves the new walk entry
+        public void SaveWalkItem_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopToRootAsync(true);
+        }
+    }
+}
---
+++ TrackMyWalks/TrackMyWalks/Views/WalksTrailInfoPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TrackMyWalks.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WalksTrailInfoPage : ContentPage
+    {
+        public WalksTrailInfoPage()
+        {
+            InitializeComponent();
+
+            // Update the page title for our Walk Information Page
+            Title = "Trail Walk Information";
+
+            // Set the Binding Context for our ContentPage
+            this.BindingContext = App.SelectedItem;
+        }
+
+        // Instance method that proceeds to begin a new walk trail
+        public void BeginTrailWalk_Clicked(object sender, EventArgs e)
+        {
+            if (App.SelectedItem == null)
+                return;
+
+            Navigation.PushAsync(new WalkDistancePage());
+            Navigation.RemovePage(this);
+        }
+    }
+}
